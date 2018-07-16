2d9512e4a22e6ef4acf228a53d18507b8ab06374
mrpete13 <35281907+mrpete13@users.noreply.github.com>
2018-07-15 22:58:55 -0400

    Add files via upload

--- pi_chart.py
+++ pi_chart.py
@@ -1,7 +1,7 @@
-import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-import pandas as pd
+# import numpy as np
+# import matplotlib as mpl
+# import pandas as pd
 
 """
 This program charts a histogram of the distribution of the digits in pi.
@@ -15,63 +15,17 @@ digit_list = []
 for digit in pi:
     digit_list.append(digit)
 
-zero = []
-one = []
-two = []
-three = []
-four = []
-five = []
-six = []
-seven = []
-eight = []
-nine = []
-
+del digit_list[1]  # This removes the '.' string
+digit_list.pop()  # This removes the '\n' string
 for digit in digit_list:
-    if digit == '0':
-        zero.append(digit)
-    elif digit == '1':
-        one.append(digit)
-    elif digit == '2':
-        two.append(digit)
-    elif digit == '3':
-        three.append(digit)
-    elif digit == '4':
-        four.append(digit)
-    elif digit == '5':
-        five.append(digit)
-    elif digit == '6':
-        six.append(digit)
-    elif digit == '7':
-        seven.append(digit)
-    elif digit == '8':
-        eight.append(digit)
-    elif digit == '9':
-        nine.append(digit)
+    digit = int(digit)
 
-zero = len(zero)
-one = len(one)
-two = len(two)
-three = len(three)
-four = len(four)
-five = len(five)
-six = len(six)
-seven = len(seven)
-eight = len(eight)
-nine = len(nine)
+bins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-digit_count = [zero, one, two, three, four, five, six, seven, eight, nine]
+plt.hist(digit_list, bins, histtype='bar', rwidth=0.5)
 
-for digit in digit_count:
-    print(digit_count.index() + ' : ' + digit)
+plt.title('Distribution of Digits in Pi')
+plt.xlabel('Digits')
+plt.ylabel('Times appeared in the first million digits of Pi')
 
-# bins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-# plt.hist(digit_list, bins, histtype = 'bar', rwidth = 0.5)
-#
-#
-#
-# plt.title('Distribution of Digits in Pi')
-# plt.xlabel('Digits')
-# plt.ylabel('Times appeared in the first million digits of pi')
-#
-# plt.show()
+plt.show()
